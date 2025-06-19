a93670745c8bbb756c212b2e0b3483b39391b14f
Reena-evanjaline29 <evanjalineraceinnovations@gmail.com>
2025-06-19 16:29:48 +0530

    correction

--- app/sfp2/Home.js
+++ app/sfp2/Home.js
@@ -4,7 +4,7 @@ import React from 'react';
 import Footer from '../components/Footer';
 import Navbar from '../components/Navbar';
 
-import Sfp2 from './SFP2';
+import Sfp2 from './Sfp2';
 
 const Home = () => {
   return (
